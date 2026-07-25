9aec3ab0d4ee01ae9845fdae1981b9c78a5a17b3
shakib <mohammedshakib663@gmail.com>
2026-07-25 14:26:56 +0530

    todays java practice

---
+++ java/demo1.java
@@ -0,0 +1,34 @@
+public class demo1 {
+    public static void main(String[] args) {
+        /*
+         * System.out.println("start the program");
+         * System.out.println("line 1");
+         * System.out.println("line 2");
+         * System.out.println("line 3");
+         * System.out.println("End of the program");
+         */
+        boolean isVerified = false;
+        if (isVerified) {
+            System.out.println("user is not verified");
+        }
+
+        int input = 10;
+        switch (input) {
+            case 1:
+                System.out.println("line 1");
+                break;
+            case 2:
+                System.out.println("line 2");
+            case 3:
+                System.out.println("line 3");
+            case 4:
+                System.out.println("line 4");
+            case 5:
+                System.out.println("line 4");
+            default:
+                System.out.println("not defined");
+
+        }
+
+    }
+}
---
+++ java/smarAtm.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class smarAtm {
+    public static void main(String args[]) {
+        int fixedCode = 1111;
+        int pin, attempts = 3;
+        int balance = 5000;
+        double depositAmount;
+        double withdrawAmount;
+        Scanner sc = new Scanner(System.in);
+        while (attempts > 0) {
+            System.out.println("Enter your pin:");
+            pin = sc.nextInt();
+            if (pin == fixedCode) {
+                break;
+            } else {
+                attempts -= 1;
+                System.out.println("Incorrect pin");
+                System.out.println("Attempts left:" + attempts);
+            }
+        }
+        if (attempts == 0) {
+            System.out.println("Your card is blocked:");
+            return;
+        }
+        while (true) {
+            System.out.println("Welcome to the atm:");
+            System.out.println("1.check your balance:");
+            System.out.println("2.deposit your money:");
+            System.out.println("3.withdraw the amount:");
+            System.out.println("4.Exit");
+            System.out.println("Enter your choice:");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("check your balance:" + balance);
+                    break;
+                case 2:
+                    System.out.println("deposit your money:");
+                    depositAmount = sc.nextDouble();
+                    balance += depositAmount;
+                    break;
+                case 3:
+                    System.out.println("withdrawl your amount:");
+                    withdrawAmount = sc.nextDouble();
+                    if (withdrawAmount > balance) {
+                        System.out.println("Insufficient balance:");
+                    } else {
+                        System.out.println("collect your money:");
+                    }
+                    break;
+                case 4:
+                    System.out.println("exit");
+                    return;
+                default:
+                    System.out.println("incorrect pin");
+
+            }
+        }
+
+    }
+}
