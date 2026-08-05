d0d250b9b46f340b7981bf851162229cfd90e36a
MohammedShakib-645 <5645mohammedshakib@gmail.com>
2026-08-05 23:44:21 +0530

    j

--- java/demo3.java
+++ java/demo3.java
@@ -1,39 +1,40 @@
 public class demo3 {
     public static void main(String[] args) {
-       /*  double values[]=new double[10];
-        float values1[]=new float[10];
-
-        for(double i:values){
-            System.out.println(i);
-        }
-        for(float i:values1){
-            System.out.println(i);
-        }*/
-
-       /*
-       int values[]={1,2,3,4,5,6,7};
-       for(int count=0;count<values.length;count++){
-        System.out.println(values[count]);
-        */
-       //--------------------------------------
-       //=======================================
-       /*float values[]={1,2,3,4,5,6,7};
-       for(int count=0;count<values.length;count++)
-       {
-          System.out.println(values[count]);
-       }
-          */
-         
         /*
-       double values[]={77,2,3,4,5,6,7,8,9,10};
-       for (int count=0;count<values.length;count++)
-       {
-        System.out.println(values[count]);
-       }
-       */
-      //-==========================================
-        
-scanner scan=new scanner 
+         * double values[]=new double[10];
+         * float values1[]=new float[10];
+         * 
+         * for(double i:values){
+         * System.out.println(i);
+         * }
+         * for(float i:values1){
+         * System.out.println(i);
+         * }
+         */
+
+        /*
+         * int values[]={1,2,3,4,5,6,7};
+         * for(int count=0;count<values.length;count++){
+         * System.out.println(values[count]);
+         */
+        // --------------------------------------
+        // =======================================
+        /*
+         * float values[]={1,2,3,4,5,6,7};
+         * for(int count=0;count<values.length;count++)
+         * {
+         * System.out.println(values[count]);
+         * }
+         */
+
+        /*
+         * double values[]={77,2,3,4,5,6,7,8,9,10};
+         * for (int count=0;count<values.length;count++)
+         * {
+         * System.out.println(values[count]);
+         * }
+         */
+        // -==========================================
 
     }
 }
